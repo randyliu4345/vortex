13b8c28e6a8cdbc9057635556a66a5d42441f57e
Alex <anppgtao@gmail.com>
2025-08-12 15:47:56 -0700

    optimized the BSS part for the vecadd

--- tests/regression/vecadd/kernel.cpp
+++ tests/regression/vecadd/kernel.cpp
@@ -14,11 +14,31 @@
 int main() {
 	kernel_arg_t* arg = (kernel_arg_t*)csr_read(VX_CSR_MSCRATCH);
 	// return vx_spawn_threads(1, &arg->num_points, nullptr, (vx_kernel_func_cb)kernel_body, arg);
-	size_t warpId = csr_read(VX_CSR_CTA_ID);
-	size_t warpSize = csr_read(VX_CSR_NUM_THREADS);
-	size_t threadOff = csr_read(VX_CSR_THREAD_ID);
+	int warpId = static_cast<int>(csr_read(VX_CSR_CTA_ID));
+	int warpSize = vx_num_threads();
+	int threadId = vx_thread_id();
 
-	size_t threadId = warpId * warpSize + threadOff;
+	int cond = (warpId == 0) && (threadId == 0);
+	int sp = vx_split(cond);
+
+	if (cond) {
+		asm volatile(
+			"la   a0, _edata\n\t"
+			"la   a2, _end\n\t"
+			"sub  a2, a2, a0\n\t"
+			"li   a1, 0\n\t"
+			"call memset\n\t"
+			:
+			:
+			: "memory", "ra",
+				"a0","a1","a2","a3","a4","a5","a6","a7",
+				"t0","t1","t2","t3","t4","t5","t6");
+	}
+
+	vx_join(sp);
+
+
+	int g_threadId = warpId * warpSize + threadId;
 
 	// vx_printf("thread id: %d\n", threadId);
 
@@ -26,7 +46,7 @@ int main() {
 	auto src1_ptr = reinterpret_cast<TYPE*>(arg->src1_addr);
 	auto dst_ptr  = reinterpret_cast<TYPE*>(arg->dst_addr);
 
-	dst_ptr[threadId] = src0_ptr[threadId] + src1_ptr[threadId];
+	dst_ptr[g_threadId] = src0_ptr[g_threadId] + src1_ptr[g_threadId];
 
 	return 0;
 }
